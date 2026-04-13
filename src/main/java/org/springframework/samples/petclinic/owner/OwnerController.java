7f6879834a39cc8518b7041b2b13804c161168af
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-04-13 16:21:54 +0200

    stub: findOwner method placeholder

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -61,10 +61,7 @@ class OwnerController {
 
 	@ModelAttribute("owner")
 	public Owner findOwner(@PathVariable(name = "ownerId", required = false) Integer ownerId) {
-		return ownerId == null ? new Owner()
-				: this.owners.findById(ownerId)
-					.orElseThrow(() -> new IllegalArgumentException("Owner not found with id: " + ownerId
-							+ ". Please ensure the ID is correct " + "and the owner exists in the database."));
+		throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	@GetMapping("/owners/new")

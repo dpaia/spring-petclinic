b8c181cb7fe425b13e16a00c5cadc4d5c7a01c02
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-04-13 15:54:39 +0200

    stub: processFindForm method placeholder

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -92,27 +92,7 @@ class OwnerController {
 	@GetMapping("/owners")
 	public String processFindForm(@RequestParam(defaultValue = "1") int page, Owner owner, BindingResult result,
 			Model model) {
-		// allow parameterless GET request for /owners to return all records
-		if (owner.getLastName() == null) {
-			owner.setLastName(""); // empty string signifies broadest possible search
-		}
-
-		// find owners by last name
-		Page<Owner> ownersResults = findPaginatedForOwnersLastName(page, owner.getLastName());
-		if (ownersResults.isEmpty()) {
-			// no owners found
-			result.rejectValue("lastName", "notFound", "not found");
-			return "owners/findOwners";
-		}
-
-		if (ownersResults.getTotalElements() == 1) {
-			// 1 owner found
-			owner = ownersResults.iterator().next();
-			return "redirect:/owners/" + owner.getId();
-		}
-
-		// multiple owners found
-		return addPaginationModel(page, model, ownersResults);
+		throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	private String addPaginationModel(int page, Model model, Page<Owner> paginated) {

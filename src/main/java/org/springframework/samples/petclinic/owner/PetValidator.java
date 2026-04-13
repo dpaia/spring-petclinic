fe10eb0d014c3bb39c4ae0a6c35fb36ac4936c5f
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-04-13 16:28:15 +0200

    stub: validate method placeholder

--- src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
@@ -35,22 +35,7 @@ public class PetValidator implements Validator {
 
 	@Override
 	public void validate(Object obj, Errors errors) {
-		Pet pet = (Pet) obj;
-		String name = pet.getName();
-		// name validation
-		if (!StringUtils.hasText(name)) {
-			errors.rejectValue("name", REQUIRED, REQUIRED);
-		}
-
-		// type validation
-		if (pet.isNew() && pet.getType() == null) {
-			errors.rejectValue("type", REQUIRED, REQUIRED);
-		}
-
-		// birth date validation
-		if (pet.getBirthDate() == null) {
-			errors.rejectValue("birthDate", REQUIRED, REQUIRED);
-		}
+		throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	/**

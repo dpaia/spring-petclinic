b371c68f3b36e1cc8e30c2b4eb0b5b0517d6112e
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-06-02 21:05:57 +0200

    Restore MySQL Testcontainers driver

--- src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
+++ src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
@@ -33,14 +33,14 @@ import org.testcontainers.utility.DockerImageName;
 public class MysqlTestApplication {
 
 	@ServiceConnection
-	@Profile("mysql")
+	@Profile("mysql-container")
 	@Bean
 	static MySQLContainer<?> container() {
 		return new MySQLContainer<>(DockerImageName.parse("mysql:9.2"));
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, "--spring.profiles.active=mysql",
+		SpringApplication.run(PetClinicApplication.class, "--spring.profiles.active=mysql,mysql-container",
 				"--spring.docker.compose.enabled=false");
 	}
 

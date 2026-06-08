160671a58c28bcc5c51bcf98e41ae8b144312ecb
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-06-08 12:22:01 +0200

    Fix owner email fixtures and i18n

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -113,9 +113,10 @@ class ClinicServiceTests {
 		Owner owner = new Owner();
 		owner.setFirstName("Sam");
 		owner.setLastName("Schultz");
-		owner.setAddress("4, Evans Street");
+		owner.setAddress("4 Evans Street");
 		owner.setCity("Wollongong");
 		owner.setTelephone("4444444444");
+		owner.setEmail("sam.schultz@example.com");
 		this.owners.save(owner);
 		assertThat(owner.getId()).isNotZero();
 
